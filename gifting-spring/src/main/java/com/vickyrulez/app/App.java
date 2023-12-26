571a97ffbc3a1cfcd1a9f4205d72f53a4100a847
victoriaprisco <p.victoria@wustl.edu>
2023-12-26 18:28:26 -0500

    setup

---
+++ gifting-spring/src/main/java/com/vickyrulez/app/App.java
@@ -0,0 +1,13 @@
+package com.vickyrulez.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
